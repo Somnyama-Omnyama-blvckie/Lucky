5b81b433f18d0181c808ca68c5ef1821c33f1352
Somnyama-Omnyama-blvckie <160587836+Somnyama-Omnyama-blvckie@users.noreply.github.com>
2024-02-20 11:03:28 +0200

    Add files via upload

---
+++ my vali vali.js
@@ -0,0 +1,6 @@
+document.getElementById("valentineBtn").addEventListener("click", function() {
+    document.getElementById("valentineMessage").classList.remove("hidden");
+    this.style.display = "none";
+});
+
+
